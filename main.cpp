2c4df1411c821ec895c58cd14eed02301d1686d5
aliarca <alice.arcardini@mail.polimi.it>
2021-03-11 13:24:31 +0000

    Include my class from the main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "subpro/my_class.hpp"
 
 int main(int argc, chat **argv)
 {
 std::cout<<"Hello, world" << std::endl;
+std::cout<<"This is a newly added line" << std::endl;
 
 return 0;
 }
